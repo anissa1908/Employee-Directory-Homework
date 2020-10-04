ae933597e0ac43d73f352fa31274a2f8d2584b4f
Anissa Shanks <anissa.shanks@gmail.com>
2020-10-04 18:31:51 -0500

    pushing to Heroku

--- employeedirectory/src/components/Table/Table.js
+++ employeedirectory/src/components/Table/Table.js
@@ -4,19 +4,23 @@ import axios from "axios";
 function Table (){
     const [users, setUsers] = useState([]);
 // enter the coding for useEffect here so that the api call is only made once and does not keep refreshing.
+//useEffect(() => {
+//     fetch("localhost:3001/links/")
+//         .then(Response => response.json())
+//         .then(data => setData(data));
+// }, []);
     axios.get("https://randomuser.me/api/?nat=us&results=200")
     .then(({data})=> setUsers(data.results));
 
     return (
         <table class="table table-dark">
   <thead>
-    <tr>
-        
+    <tr>  
       <th scope="col"></th>
       <th scope="col">First Name</th>
       <th scope="col">Last Name</th>
-      <th scope="col">Email</th>
       <th scope="col">Phone</th>
+      <th scope="col">Email</th>
     </tr>
   </thead>
   <tbody>
